6735eeea83199fc17d287abd612d3c5605d11e17
mustard <mhqnwt@gmail.com>
2022-10-10 19:07:03 +0000

    using nodejs server

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 let content = 'tasks:\n'
 
 for (let i = 0; i < 123; i++) {
-    content += `  - command: LAMA_PORT=${8000+i} ./lama.sh\n`
+    content += `  - command: SERVER_PORT=${8000+i} node server.js\n`
 }
 
 require('fs').writeFileSync('./.gitpod.yml', content)
