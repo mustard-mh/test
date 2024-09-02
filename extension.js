c21931cace50da3e906114a6f16074f7999e50ac
Huiwen <mhqnwt@gmail.com>
2024-09-02 06:28:59 +0000

    Simple test case

--- extension.js
+++ extension.js
@@ -1,14 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { exec } = require('child_process');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
 function activate() {
-	for (let i = 0; i < 3; i++) {
-		vscode.commands.executeCommand('simpleBrowser.api.open', 'https://www.google.com/');
-	}
+	const cmd = `gp preview --external https://google.com`
+	const env = {...process.env}
+	// delete env["ELECTRON_RUN_AS_NODE"]
+	exec(cmd, { env }, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
 }
 
 // This method is called when your extension is deactivated
